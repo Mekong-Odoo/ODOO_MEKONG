06874dba958f4b675cd79832f6500d4d4ac74f3b
TranDangMyTien <mytien.2682003@gmail.com>
2025-04-28 11:09:31 +0700

    Initial commit

---
+++ server/custom_modules/company_profile/__manifest__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'Company Profile',
+    'version': '1.0',
+    'summary': 'Quản lý thông tin doanh nghiệp',
+    'description': 'Module dùng để lưu trữ và quản lý hồ sơ công ty như tên, mã số thuế, lĩnh vực hoạt động...',
+    'category': 'Custom',
+    'author': 'Mỹ Tiên',
+    'website': 'https://yourwebsite.com',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        # 'views/company_profile_view.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
